32ee37cbdd2e658dbb954e4b9c9660b8921d67d0
dougnutz <dougnutz@users.noreply.github.com>
2022-05-16 17:45:20 -0400

    Update gateDev.cpp (#3)

--- gate/src/gateDev.cpp
+++ gate/src/gateDev.cpp
@@ -10,7 +10,7 @@ const int green =5 ;
 char ssid[] = "LHome";     //  your network SSID (name)
 char pass[] = "DougAndErikaWireless";  // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
-char gateUrl[]="http://dtipsstore.table.core.windows.net/gate(PartitionKey='gate',RowKey='state')?sv=2019-12-12&ss=t&srt=sco&sp=rwdlacu&se=2022-01-22T03:04:00Z&st=2021-01-20T19:04:00Z&spr=https,http&sig=CpEls%2FxLL5dYPLnbcnEnXBBMngsDlWcwLcfW6twdmwg%3D";
+char gateUrl[]="http://dtipsstore.table.core.windows.net/gate(PartitionKey='gate',RowKey='state')?sv=";
 
 unsigned int count = 1;
 void setup() {
