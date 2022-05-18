648e1a370caefb771c0ab3a15316af11322f8f34
Doug Lewis <dolewis@microsoft.com>
2022-05-18 15:19:53 -0400

    hellow world with lib

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,9 +1,112 @@
 #include <Arduino.h>
 
+/**
+   The MIT License (MIT)
+
+   Copyright (c) 2018 by ThingPulse, Daniel Eichhorn
+   Copyright (c) 2018 by Fabrice Weinberg
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+   ThingPulse invests considerable time and money to develop these open source libraries.
+   Please support us by buying our products (and not the clones) from
+   https://thingpulse.com
+
+*/
+
+// For a connection via I2C using the Arduino Wire include:
+//#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
+
+// Optionally include custom images
+// #include "images.h"
+
+
+// Initialize the OLED display using Arduino Wire:
+ SSD1306Wire display(0x3c, D3, D5);  // ADDRESS, SDA, SCL  
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+
+  // Initialising the UI will init the display too.
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+
 }
 
+
+void drawRectDemo() {
+  // Draw a pixel at given position
+  for (int i = 0; i < 10; i++) {
+    display.setPixel(i, i);
+    display.setPixel(10 - i, i);
+  }
+  display.drawRect(12, 12, 20, 20);
+
+  // Fill the rectangle
+  display.fillRect(14, 14, 17, 17);
+
+  // Draw a line horizontally
+  display.drawHorizontalLine(0, 40, 20);
+
+  // Draw a line horizontally
+  display.drawVerticalLine(40, 0, 20);
+}
+
+void drawCircleDemo() {
+  for (int i = 1; i < 8; i++) {
+    display.setColor(WHITE);
+    display.drawCircle(32, 32, i * 3);
+    if (i % 2 == 0) {
+      display.setColor(BLACK);
+    }
+    display.fillCircle(96, 32, 32 - i * 3);
+  }
+}
+
+int counter = 1;
+void drawProgressBarDemo() {
+  counter ++;
+  int progress = (counter / 5) % 100;
+  // draw the progress bar
+  display.drawProgressBar(0, 32, 120, 10, progress);
+
+  // draw the percentage as String
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 15, String(progress) + "%");
+}
+
+
+//Demo demos[] = {, drawRectDemo, drawCircleDemo, drawProgressBarDemo};
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // clear the display
+  display.clear();
+
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+ // display.setFont(ArialMT_Plain_16);
+ // display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 26, "Hello world");
+
 }
