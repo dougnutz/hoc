2837c83274117f71c5fef981a9fd2c41827f6b75
Doug Lewis <dolewis@microsoft.com>
2022-11-07 13:31:16 -0500

    lightswtich controller

--- LightSwitch/TurnOnOrOff/src/main.cpp
+++ LightSwitch/TurnOnOrOff/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <Arduino.h>
+// #include "SSD1306Wire.h"  
+
+char ssid[] = "SSID";     //  your network SSID (name)
+char pass[] = "<wifi password>";  // your network password
+char gateUrl[]="http://hocfun.azurewebsites.net/api/echo?code=<code>&name=hello";
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.println("Setup begin");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+     Serial.print("Begin Loop ... ");
+
+
+    //wait 5 sec and check again 
+    delay(5000);
 }
