0bc697018e108dff63ad38edf3cac736883bbe35
Doug <dolewis@microsoft.com>
2023-03-02 10:26:29 -0500

    initial commit, hellow world checkpoint

---
+++ games/BasicMovement/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include "SSD1306Wire.h"       
+
+// Initialize the OLED display using Arduino Wire:
+ SSD1306Wire display(0x3c, D3, D5);  // ADDRESS, SDA, SCL  
+
+void setup() {
+  // initialize com port
+  Serial.begin(9600);
+  Serial.println("Starting...");
+
+  // initialize display
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+}
+
+void loop() {
+  display.clear();
+  display.drawString(0, 0, "Hello World");
+  display.display();
+  delay(2000);
+}
