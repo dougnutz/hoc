dcddae487473ea6bc0dfa9195929fad7fc0b0781
Doug <dolewis@microsoft.com>
2023-03-30 09:55:59 -0400

    finalize basic movement

--- games/BasicMovement/src/main.cpp
+++ games/BasicMovement/src/main.cpp
@@ -1,14 +1,8 @@
 #include <Arduino.h>
-#include "SSD1306Wire.h"    
-#include <Bounce2.h>   
+#include "SSD1306Wire.h"       
 
 // Initialize the OLED display using Arduino Wire:
  SSD1306Wire display(0x3c, D3, D5);  // ADDRESS, SDA, SCL  
- int lineWidth = 20;
- int linePosition = 0;
- int direction = 1;
- Bounce2::Button button = Bounce2::Button();
- int buttonPin = D1;
 
 void setup() {
   // initialize com port
@@ -17,38 +11,14 @@ void setup() {
 
   // initialize display
   display.init();
-
-  button.attach( buttonPin, INPUT_PULLUP ); 
-  button.interval(5); 
-  button.setPressedState(LOW); 
-}
-
-void drawLine(  int x, int length){
-  Serial.println("draw line...");
-  display.drawHorizontalLine(x, 63, length);
-  display.display();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
 }
 
 void loop() {
- 
-  Serial.println("Begin loop...");
   display.clear();
-
-  button.update();
-    if (button.pressed()){
-      Serial.println("button pressed");
-      direction = direction * -1;
-    }
-
-  if (linePosition < 128-lineWidth && direction == 1){
-    drawLine(++linePosition, lineWidth);
-     
-  }
-  else if(linePosition >0 && direction == -1){
-    drawLine(--linePosition, lineWidth);
-  }
-  
-
-  Serial.println("linePosition: " + String(linePosition));
-  delay(200);
+  display.drawString(0, 0, "Hello World");
+  display.display();
+  delay(2000);
 }
